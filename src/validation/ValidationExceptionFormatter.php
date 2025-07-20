988dac195853be5cfb9df7a2e5e2b00144da036d
Adriano Miranda <adrjmiranda@gmail.com>
2025-07-20 16:07:22 -0300

    docs: created documentation validation exception formatter class

--- src/validation/ValidationExceptionFormatter.php
+++ src/validation/ValidationExceptionFormatter.php
@@ -4,8 +4,19 @@ namespace LpApi\Validation;
 
 use Respect\Validation\Exceptions\NestedValidationException;
 
+/**
+ * Class ValidationExceptionFormatter
+ *
+ * Provides helper methods to format validation exception messages.
+ */
 class ValidationExceptionFormatter
 {
+  /**
+   * Formats the messages from a NestedValidationException.
+   *
+   * @param NestedValidationException $e The exception to format.
+   * @return string[] An array of formatted error messages.
+   */
   public static function formatMessages(NestedValidationException $e): array
   {
     return $e->getMessages();
