4cd42fc2c241c49ed56982bfe9894c0ca021edc4
Adriano Miranda <adrjmiranda@gmail.com>
2025-07-21 14:31:35 -0300

    docs: created documentation for mailer validator class

--- src/validation/MailerValidator.php
+++ src/validation/MailerValidator.php
@@ -2,6 +2,15 @@
 
 namespace LpApi\Validation;
 
+/**
+ * MailerValidator
+ * 
+ * Extends DefaultValidator to provide validation rules specific
+ * to mailer-related data. Use this class to add and run validations
+ * for email form input or other mail-related fields.
+ * 
+ * Inherits all functionality from DefaultValidator.
+ */
 class MailerValidator extends DefaultValidator
 {
 }
